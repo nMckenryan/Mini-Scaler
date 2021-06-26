c6d0d28ec12d59f8add1b333227cce84dd9a9e3d
Nigel Mckenzie-Ryan <n.mckenryan@gmail.com>
2021-06-26 17:51:58 +1000

    Implemented a logo and moved the menu to the main application to allow for more functionality without the use with props. going to make the menus more modular by making a constructor for them.

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,5 @@
 <template>
   <v-app>
-    <!--  -->
-    <v-app-bar app color="light-blue darken-4" dark>
-      <div class="d-flex align-center">
-        <!-- TODO: Create Logo -->
-        <v-img
-          alt="MiniscalerLogo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <h1 class="display-1 font-weight-bold mb-3">
-          Mini Scaler
-        </h1>
-      </div>
-      <v-spacer></v-spacer>
-      <!-- TODO: Add reference Info dropdown bar -->
-
-      <v-menu transition="slide-y-transition" bottom>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn class="purple" color="primary" dark v-bind="attrs" v-on="on">
-            40k Reference
-          </v-btn>
-        </template>
-        <v-list>
-          <v-list-item v-for="(item, i) in Wh40Heights" :key="i">
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-
-      <v-menu transition="slide-y-transition" bottom>
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn class="purple" color="primary" dark v-bind="attrs" v-on="on">
-            DnD Reference
-          </v-btn>
-        </template>
-        <v-list>
-          <v-list-item v-for="(item, i) in dndHeights" :key="i">
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </v-app-bar>
-
     <v-main>
       <ScaleCalculator />
     </v-main>
@@ -63,29 +16,6 @@ export default {
     ScaleCalculator,
   },
 
-  data: () => ({
-    dndHeights: [
-      { title: "Human: 67'" },
-      { title: "Half Elf: 66'" },
-      { title: "Elf: 65'" },
-      { title: "Dwarf: 49'" },
-      { title: "Halfling: 36' " },
-      { title: "Gnome: 40' " },
-      { title: "Half-Orc: 69' " },
-      { title: "Dragonborn: 75' " },
-      { title: "Goliath: 90'" },
-    ],
-    Wh40Heights: [
-      { title: "Imperial Guardsman: 6 ft" },
-      { title: "Space Marine (Armoured): 7'5 ft" },
-      { title: "Ork: 6 - 7ft" },
-      { title: "Ork Warboss: 10+ ft" },
-      { title: "Genestealer: 6'2ft " },
-      { title: "Eldar/Drukhari: 6'2 ft" },
-      { title: "Necron: 8 ft" },
-      { title: "Tau: 5'5 ft" },
-      { title: "Tyranid Warrior: 9 ft" },
-    ],
-  }),
+  data: () => ({}),
 };
 </script>
--- src/components/ScaleCalculator.vue
+++ src/components/ScaleCalculator.vue
@@ -1,33 +1,85 @@
 <template>
-  <v-container width="200" class="mx-auto mt-5">
-    <v-row class="text-center">
-      <v-col class="mb-5 mx-auto" cols="8">
-        <v-form>
-          <v-layout row wrap>
-            <!-- TODO: Instantiate Imperial Conversion -->
+  <v-app>
+    <v-app-bar app color="light-blue darken-4" dark>
+      <div class="d-flex align-center">
+        <!-- TODO: Create Logo -->
+        <v-img
+          alt="MiniscalerLogo"
+          class="shrink mr-2"
+          contain
+          src="..\assets\scalerLogoWhite.png"
+          transition="scale-transition"
+          width="45"
+        />
 
-            <v-flex s2>
-              <v-text-field
-                label="Character Height (Centimetres)"
-                prepend-icon="mdi-tape-measure"
-                v-model.number="realCm"
-                type="number"
-                suffix="cm"
-              ></v-text-field>
-            </v-flex>
-            <v-spacer></v-spacer>
+        <h1 class="display-1 font-weight-bold my-auto">
+          Mini Scaler
+        </h1>
+      </div>
+      <v-spacer></v-spacer>
+      <!-- TODO: Add reference Info dropdown bar -->
 
-            <v-flex s2>
-              <v-text-field
-                label="Character Height (Metres)"
-                prepend-icon="mdi-tape-measure"
-                v-model.lazy="update"
-                type="number"
-                suffix="m"
-              ></v-text-field>
-            </v-flex>
+      <v-menu transition="slide-y-transition" bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn class="purple" color="primary" dark v-bind="attrs" v-on="on">
+            40k Reference
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="(item, i) in Wh40Heights" :key="i">
+            <v-list-item-name @click="this.realCm = item.height">
+              {{ item.name }} {{ item.height }} cm</v-list-item-name
+            >
+          </v-list-item>
+        </v-list>
+      </v-menu>
 
-            <!-- <v-flex s2>
+      <!-- TODO: Change CharHeight on click -->
+      <v-menu transition="slide-y-transition" bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn class="purple" color="primary" dark v-bind="attrs" v-on="on">
+            DnD Reference
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="(item, i) in dndHeights" :key="i">
+            <v-list-item-name
+              >{{ item.name }} {{ item.height }} cm
+            </v-list-item-name>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+
+    <v-container width="200" class="mx-auto mt-5">
+      <v-row class="text-center">
+        <v-col class="mb-5 mx-auto" cols="8">
+          <v-form>
+            <v-layout row wrap>
+              <!-- TODO: Instantiate Imperial Conversion -->
+
+              <v-flex s2>
+                <v-text-field
+                  label="Character Height (Centimetres)"
+                  prepend-icon="mdi-tape-measure"
+                  v-model.number="realCm"
+                  type="number"
+                  suffix="cm"
+                ></v-text-field>
+              </v-flex>
+              <v-spacer></v-spacer>
+
+              <v-flex s2>
+                <v-text-field
+                  label="Character Height (Metres)"
+                  prepend-icon="mdi-tape-measure"
+                  v-model.lazy="update"
+                  type="number"
+                  suffix="m"
+                ></v-text-field>
+              </v-flex>
+
+              <!-- <v-flex s2>
               <v-text-field
                 label="Character Height (Inches)"
                 prepend-icon="mdi-tape-measure"
@@ -38,38 +90,38 @@
                 v-on:update="scaleToMini"
               ></v-text-field>
             </v-flex> -->
-          </v-layout>
+            </v-layout>
 
-          <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
 
-          <!-- Scale Selection -->
-          <v-btn-toggle
-            v-model="scaleType"
-            tile
-            color="deep-blue accent-3"
-            group
-          >
-            <v-btn value="28">
-              28mm
-            </v-btn>
+            <!-- Scale Selection -->
+            <v-btn-toggle
+              v-model="scaleType"
+              tile
+              color="deep-blue accent-3"
+              group
+            >
+              <v-btn value="28">
+                28mm
+              </v-btn>
 
-            <v-btn value="32">
-              32mm
-            </v-btn>
-          </v-btn-toggle>
+              <v-btn value="32">
+                32mm
+              </v-btn>
+            </v-btn-toggle>
 
-          <v-text-field
-            label="Printed Mini Size"
-            v-model.lazy="scaleToMini"
-            prepend-icon="mdi-scale-balance"
-            type="number"
-            suffix="mm"
-            readonly
-          ></v-text-field>
-        </v-form>
-      </v-col>
-    </v-row>
-  </v-container>
+            <v-text-field
+              label="Printed Mini Size"
+              v-model.lazy="scaleToMini"
+              prepend-icon="mdi-scale-balance"
+              type="number"
+              suffix="mm"
+              readonly
+            ></v-text-field>
+          </v-form>
+        </v-col>
+      </v-row> </v-container
+  ></v-app>
 </template>
 
 <script>
@@ -84,6 +136,29 @@ export default {
     realM: 0,
     scaleType: 28,
     scaleHeight: 0,
+
+    dndHeights: [
+      { name: "Human", height: 170 },
+      { name: "Half Elf", height: 167 },
+      { name: "Elf", height: 165 },
+      { name: "Dwarf", height: 125 },
+      { name: "Halfling", height: 91 },
+      { name: "Gnome", height: 101 },
+      { name: "Half-Orc", height: 175 },
+      { name: "Dragonborn", height: 190 },
+      { name: "Goliath", height: 228 },
+    ],
+    Wh40Heights: [
+      { name: "Imperial Guardsman", height: 182 },
+      { name: "Space Marine (Armoured)", height: 228 },
+      { name: "Ork ", height: 198 },
+      { name: "Ork Warboss ", height: 300 },
+      { name: "Genestealer ", height: 189 },
+      { name: "Eldar/Drukhari ", height: 189 },
+      { name: "Necron ", height: 243 },
+      { name: "Tau", height: 167 },
+      { name: "Tyranid Warrior", height: 274 },
+    ],
   }),
 
   computed: {
