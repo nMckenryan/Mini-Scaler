6faadeb09479c9c27faaff460769b1d021c4cf6a
Nigel Mckenzie-Ryan <n.mckenryan@gmail.com>
2021-07-01 13:50:43 +1000

    Update ScaleCalculator.vue

--- src/components/ScaleCalculator.vue
+++ src/components/ScaleCalculator.vue
@@ -2,11 +2,11 @@
   <v-container width="200" class="mx-auto mt-5">
     <v-row class="text-center">
       <v-col class="mb-5 mx-auto" cols="8">
-        <!-- TOGGLE IMPERIAL/METRIC
-        <v-switch v-model="isImp" inset :label="label"></v-switch> -->
+        <!-- TOGGLE IMPERIAL/METRIC -->
+        <v-switch v-model="isImp" inset :label="label"></v-switch>
 
         <v-form>
-          <v-layout row wrap>
+          <v-layout row wrap v-if="isImp == false">
             <!-- CENTIMETRES -->
             <v-flex s2>
               <v-text-field
@@ -31,7 +31,8 @@
             </v-flex>
           </v-layout>
 
-          <!-- <v-layout row wrap v-else>
+          <v-layout row wrap v-else>
+            <!-- INCHES  -->
             <v-flex s2>
               <v-text-field
                 label="Character Height (Inches)"
@@ -44,6 +45,7 @@
 
             <v-spacer></v-spacer>
 
+            <!-- FEET -->
             <v-flex s2>
               <v-text-field
                 label="Character Height (Feet)"
@@ -53,7 +55,7 @@
                 suffix="ft"
               ></v-text-field>
             </v-flex>
-          </v-layout> -->
+          </v-layout>
 
           <v-spacer></v-spacer>
 
@@ -97,7 +99,14 @@ export default {
   data: () => ({
     masterMeasure: 0,
     realCm: 0,
+<<<<<<< HEAD
     realI: 0,
+=======
+    realM: 0,
+    realI: 0,
+    realF: 0,
+    isImp: false,
+>>>>>>> parent of 32a017c (REversed last change. need to focus on the MVP, which is the metric version)
     scaleType: 28,
     scaleHeight: 0,
   }),
@@ -105,28 +114,29 @@ export default {
   watch: {
     hPreset: function() {
       //Handles Top menu preset changes.
-      // if (this.isImp == false) {
       this.realCm = this.hPreset;
-      // } else {
-      //   this.realI = this.convertInch(this.hPreset);
-      // }
     },
 
     realCm: function() {
+<<<<<<< HEAD
       this.convertToInch();
+=======
+      this.realM = this.realCm / 100;
+      // this.realI = this.convertImp(this.realCm);
+>>>>>>> parent of 32a017c (REversed last change. need to focus on the MVP, which is the metric version)
     },
 
     realI: function() {
       this.convertToCm();
     },
 
-    // realI: function() {
-    //   this.convertInch(this.realI);
-    // },
+    realI: function() {
+      this.convertInch(this.realI);
+    },
 
-    // realF: function() {
-    //   this.convertFt(this.realF);
-    // },
+    realF: function() {
+      this.convertFt(this.realF);
+    },
   },
 
   computed: {
@@ -158,10 +168,39 @@ export default {
       this.realCm = this.realInch * 2.54;
     },
 
-    // label: function() {
-    //   const l = this.isImp == true ? "Imperial" : "Metric";
-    //   return l;
-    // },
+    label: function() {
+      const l = this.isImp == true ? "Imperial" : "Metric";
+      return l;
+    },
   },
+<<<<<<< HEAD
+=======
+
+  methods: {
+    parallelConversion(f) {
+      if (f == "i") {
+        this.realMetric = this.convertMetric(this.realImp);
+      } else if (f == "m") {
+        this.realImp = this.convertImp(this.realMetric);
+      }
+      this.scaleToMini();
+    },
+
+    convertImp(v) {
+      // 1 inch  = 2.54cm
+      this.realCm = v / 0.3937;
+    },
+
+    convertMetric(v) {
+      //1m = 3.28ft
+      this.realI = v * 3.28;
+    },
+
+    convertInch(v) {
+      // 1 inch  = 2.54cm
+      this.realCm = v / 0.3937;
+    },
+  },
+>>>>>>> parent of 32a017c (REversed last change. need to focus on the MVP, which is the metric version)
 };
 </script>
