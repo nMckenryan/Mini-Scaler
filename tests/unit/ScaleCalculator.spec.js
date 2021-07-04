b13a39f77058b83a16b6f5be92c6380587d97d5a
Nigel Mckenzie-Ryan <n.mckenryan@gmail.com>
2021-07-04 23:42:39 +1000

    Prepping for Heroku upload

---
+++ tests/unit/ScaleCalculator.spec.js
@@ -0,0 +1,9 @@
+// import ScaleCalculator from "../src/components/ScaleCalculator.vue";
+// import { mount } from "@vue/test-utils";
+
+// // yarn jest specs/ScaleCalculator.spec.js
+
+// test("mountcomponent", () => {
+//   const wrapper = mount(ScaleCalculator);
+//   expect(wrapper.html()).toMatchSnapshot();
+// });
